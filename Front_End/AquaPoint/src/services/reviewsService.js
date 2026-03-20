f2e0ed6c8c5f50c1d3be92146e2ad655fe51c612
Tiago Lança <tiagofilipe.lanca@gmail.com>
2026-03-20 10:47:40 +0000

    Created component to show user's interactions on profile page

--- Front_End/AquaPoint/src/services/reviewsService.js
+++ Front_End/AquaPoint/src/services/reviewsService.js
@@ -4,5 +4,6 @@ export const reviewsService = {
   getAll: () => api.get('/reviews'),
   getById: (id) => api.get(`/reviews/${id}`),
   getByPointId: (id) => api.get(`/reviews/aquapoint/${id}`),
+  getByUserId: (userId) => api.get(`/reviews/user/${userId}`),
   create: (data) => api.post('/reviews', data),
 }
