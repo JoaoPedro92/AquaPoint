33fc9f804bcaf8819b67f5073a134f523cab4cf9
Tiago Lança <tiagofilipe.lanca@gmail.com>
2026-04-21 23:35:19 +0100

    Refreshing user's favorite when removing favorite aquapoint on profile page

--- Front_End/AquaPoint/src/views/AreaPessoal.vue
+++ Front_End/AquaPoint/src/views/AreaPessoal.vue
@@ -93,7 +93,7 @@
 
             <div class="row g-3 ">
                 <div class="col-md-4 pe-3 mb-3" v-for="aquapoint in userPointsFavorite" :key="aquapoint.id">
-                    <FavoriteAquapointCard :aquapoint="aquapoint" :favoriteChanged="LoadAllUserFavoritePoints"/>
+                    <FavoriteAquapointCard :aquapoint="aquapoint" @favoriteChanged="LoadAllUserFavoritePoints"/>
                 </div>
                 
             </div><!-- Content row end -->
